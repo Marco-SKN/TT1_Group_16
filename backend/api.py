2627ba2f5662f8c6495cf9d2bbfb790140da8cb2
garyljj <gary.lee@u.nus.edu>
2021-09-30 13:56:02 +0800

    Add api

--- backend/api.py
+++ backend/api.py
@@ -31,11 +31,57 @@ functionalities:
 '''
 /api/products [get]- 2
 /api/categories [get]- 2
+/api/products_orderitems [get] - 4
 
 /api/products [post]- 3
-/api/products_orderitems [get] - 4
 /api/orderitems [delete] - 5
 /api/orders [put] - 6
 /api/products [put] - 6
 /api/orderitems [put] - 6
 '''
+def lst_products():
+    products = Product.query.all()
+    return [p.as_dict() for p in products]
+def lst_categories():
+    categories = Category.query.all()
+    return [c.as_dict() for c in categories]
+def lst_product_orderitems():
+    pass
+
+def add_product(json):
+    p = Product(**json)
+    db.session.add()
+
+    if (not Product.query.get(json['id'])):
+        db.session.add(item)
+        db.session.commit()
+    else:
+        return {'message':'item already exist'}
+
+def del_orderitem():
+    pass
+
+def update_order(json):
+    if (Order.query.get(id)):
+        Order.query.filter_by(id=id).update(json)
+        db.session.commit()
+        return json
+    else:
+        return {'message': 'item not found'}
+
+def update_product(json):
+    if (Product.query.get(id)):
+        Product.query.filter_by(id=id).update(json)
+        db.session.commit()
+        return json
+    else:
+        return {'message': 'item not found'}
+
+def update_orderitem(json):
+    if (Order_item.query.get(id)):
+        Order_item.query.filter_by(id=id).update(json)
+        db.session.commit()
+        return json
+    else:
+        return {'message': 'item not found'}
+
